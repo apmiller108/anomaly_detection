67e14ef3376a8408234f34ccd864d03e1bf4261d
Alex Miller <apmiller108@yahoo.com>
2017-09-23 23:34:47 -0400

    Calculate size of matrix

--- spec/anomoly_detection_spec.cr
+++ spec/anomoly_detection_spec.cr
@@ -2,7 +2,18 @@ require "./spec_helper"
 
 describe AnomolyDetection do
   it "initializes with an Array of integers" do
-    arg = [[1, 2, 3], [4, 5, 6]]
-    AnomolyDetection::EstimateGaussian.new(arg).should be_truthy
+    subject.should be_truthy
   end
+
+  it "returns the size of the X matrix" do
+    subject.call.should eq [2, 3]
+  end
+end
+
+def matrix
+  [[1, 2, 3], [4, 5, 6]]
+end
+
+def subject
+  AnomolyDetection::EstimateGaussian.new(matrix)
 end
--- src/anomoly_detection.cr
+++ src/anomoly_detection.cr
@@ -7,5 +7,21 @@ module AnomolyDetection
     def initialize(x : Array(Array(Int32)))
       @x = x
     end
+
+    def call
+      size
+    end
+
+    private def size
+      [num_columns, num_rows]
+    end
+
+    private def num_columns
+      x.size
+    end
+
+    private def num_rows
+      x.first.size
+    end
   end
 end
