9cb8f36608f6bc1603d541570afe090406c791fe
Alex Miller <apmiller108@yahoo.com>
2017-09-24 09:00:01 -0400

    Add mu and sigma2 calculation methods

--- spec/anomoly_detection_spec.cr
+++ spec/anomoly_detection_spec.cr
@@ -1,19 +1,36 @@
 require "./spec_helper"
 
+def matrix
+  [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]]
+end
+
+def subject
+  AnomolyDetection::EstimateGaussian.new(matrix)
+end
+
 describe AnomolyDetection do
   it "initializes with an Array of integers" do
     subject.should be_truthy
   end
 
-  it "returns the size of the X matrix" do
-    subject.call.should eq [2, 3]
+  describe ".call" do
+    it "returns the size of the X matrix" do
+      subject.call.should eq({
+        mu:     [2.5, 3.5, 4.5],
+        sigma2: [2.25, 2.25, 2.25],
+      })
+    end
   end
-end
 
-def matrix
-  [[1, 2, 3], [4, 5, 6]]
-end
+  describe ".mu" do
+    it "calculates the mu value for the ith feature" do
+      subject.mu.should eq [2.5, 3.5, 4.5]
+    end
+  end
 
-def subject
-  AnomolyDetection::EstimateGaussian.new(matrix)
+  describe ".sigma2" do
+    it "calculates the sigma2 value for the ith feature" do
+      subject.sigma2.should eq [2.25, 2.25, 2.25]
+    end
+  end
 end
--- src/anomoly_detection.cr
+++ src/anomoly_detection.cr
@@ -4,23 +4,42 @@ module AnomolyDetection
   class EstimateGaussian
     getter x
 
-    def initialize(x : Array(Array(Int32)))
+    def initialize(x : Array(Array(Float64)))
       @x = x
+      @mu = [] of Float64
+      @sigma2 = [] of Float64
     end
 
     def call
-      size
+      {
+        mu:     mu,
+        sigma2: sigma2,
+      }
+    end
+
+    def mu
+      return @mu unless @mu.empty?
+      @mu = num_columns.times.each_with_object([] of Float64) do |index, list|
+        list << x.sum { |row| row[index] } / num_rows
+      end
+    end
+
+    def sigma2
+      return @sigma2 unless @sigma2.empty?
+      @sigma2 = num_columns.times.each_with_object([] of Float64) do |index, list|
+        list << x.sum { |row| (row[index] - mu[index])**2 } / num_rows
+      end
     end
 
     private def size
-      [num_columns, num_rows]
-    end
-
-    private def num_columns
-      x.size
+      [num_rows, num_columns]
     end
 
     private def num_rows
+      x.size
+    end
+
+    private def num_columns
       x.first.size
     end
   end
